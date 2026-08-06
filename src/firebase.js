25c41783de549dabc57191fbed2b23e5856652f0
jhonjone1579 <jhonjone1579@gmail.com>
2026-08-06 09:15:43 +0700

    Fix firebase exports and build app

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUo9l-vq2SoG7iN4nq3qjylrC7zkvv9HE", // 💡 Quotes (" ") ခံပေးထားပါသည်
+  apiKey: "AIzaSyDUo9l-vq2SoG7iN4nq3qjylrC7zkvv9HE",
   authDomain: "thai-flashcards-feda1.firebaseapp.com",
   projectId: "thai-flashcards-feda1",
   storageBucket: "thai-flashcards-feda1.firebasestorage.app",
   messagingSenderId: "905382151667",
-  appId: "1:905382151667:web:f23f88bb68bd2911993e86"
+  appId: "1:123456789:web:f23f88bb68bd2911993e86"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Services
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
